1d3c6e4e1c159d04f01125c1e4c274888e54d675
Aayushman Sharma <075bme004aayushman@gmail.com>
2023-07-23 11:15:32 +0545

    first completion of the project and further improvements will be made...

--- src/components/CartTotal.js
+++ src/components/CartTotal.js
@@ -5,15 +5,28 @@ const CartTotal = ({cartItems}) => {
     const total= cartItems.reduce((accumulator,item)=> accumulator + item.price*item.quantity, 0);
 
   return (
-    <div style={{marginBottom: "5em", marginLeft: "2em"}}>
+    <div style={{marginBottom: "5em", marginLeft: "2em", marginRight: "2em"}}>
       <h2>Cart Total</h2>
-      <ul>
-        {cartItems.map((item, index) => (
-            <li key={index}>
-                {item.title} ({item.quantity}) : ${item.price} (each)
-            </li>
-        ))}
-      </ul>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Quantity</th>
+            <th scope="col">Sub-total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cartItems.map((item,index)=> (
+            <tr key={index}>
+            <th scope="row">{index+1}</th>
+            <td>{item.title}</td>
+            <td>{item.quantity}</td>
+            <td>${item.price * item.quantity}</td>
+          </tr>
+          ))}
+        </tbody>
+      </table>
       <h3>Total Price: ${total}</h3>
     </div>
   )
